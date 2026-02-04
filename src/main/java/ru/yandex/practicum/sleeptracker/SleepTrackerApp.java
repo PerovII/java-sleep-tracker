467ec205fc09a3f667dd22074daa1bf367570feb
PerovII <perovii45@yandex.ru>
2026-02-04 23:39:32 +0500

    Добавлена обработка enum UserChronotype

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -11,32 +11,40 @@ public class SleepTrackerApp {
 
     public static void main(String[] args) {
 
+        if (args.length == 0) {
+            System.out.println("Ошибка: не указан путь к файлу.");
+            return;
+        }
+
+        String filePath = args[0];
+
         try {
-            List<SleepingSession> sleepingSessions = new SleepLogReader().getSleepSessions("src/main/resources/sleep_log.txt");
+            List<SleepingSession> sleepingSessions = new SleepLogReader().getSleepSessions(filePath);
 
             List<Function<List<SleepingSession>, ? extends SleepAnalysisResult<?>>> functions = new ArrayList<>();
             functions.add(new NumberOfSleepSessions());
             functions.add(new MinimumDurationSession());
             functions.add(new MaximumDurationSession());
             functions.add(new AverageDurationSession());
-            functions.add(new NumberOfBadSassions());
+            functions.add(new NumberOfBadSessions());
             functions.add(new NumberOfSleeplessNights());
             functions.add(new UserChronotypeClassification());
 
             functions.stream()
                     .map(f -> f.apply(sleepingSessions))
-                    .forEach(r -> System.out.println(r.getDescription() + ": " + r.getValue()));
-
-
+                    .forEach(r -> {
+                                Object value = r.getValue();
+                                if (value instanceof UserChronotype chronotype) {
+                                    if (chronotype == UserChronotype.OWL) value = "сова";
+                                    else if (chronotype == UserChronotype.LARK) value = "жаворонок";
+                                    else if (chronotype == UserChronotype.DOVE) value = "голубь";
+                                }
+                            System.out.println(r.getDescription() + ": " + value);
+                    });
 
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Ошибка чтения файла: " + filePath);
+            System.out.println(e.getMessage());
         }
-
-
-
-
-
-
     }
 }
